614c12cdb7b32efdd8aabce000a66e5ff81977be
MarkusN02 <nordgrenmarkus02@gmail.com>
2025-09-24 11:15:33 +0200

    la till main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+
+def menu():
+    while True:
+        print("=== Menu ===")
+        print("1. Get words of wisdom")
+        print("2. Delete")
+        print("3. Save in a txt-file?")
+        print("4. Search")
+        print("5. Read all saved")
+        print("6. Randomize words of wisdom from a file")
+
+        choice = input("")
+
+        if choice == "1":
+            print("Hej")
+        elif choice == "2":
+            print("på")
+        elif choice == "3":
+            print("dig") 
+        elif choice == "4":
+            print("din")
+        elif choice == "5":
+            print("dåre")
+        elif choice == "6":
+            print("!")
+        else:
+            print("Felaktigt svar")
+            time.sleep(1)
+"""def loading(text):
+    print(text, end="", flush=True)
+    for i in range(3):
+        time.sleep(0.5)
+        print()
+
+
+if__name__=="__main__":
+    show_menu()"""
+
+menu()
